683042c4516992e7ba0035de94ffd06b9ced0ab8
zorazhong <jay450150@yahoo.com.tw>
2018-03-12 09:58:26 +0800

    class demo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(function() {
+    "use strict";
+
+    function menuscroll() {
+        var $navmenu = $('.nav-menu');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
+    }
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
+}); 
