050a33fd383733c5dd6f8f664664150d73758020
hsi1101 <light090424@hotmail.com>
2018-05-25 18:17:45 +0800

    MAYO

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 $(function() {
     "use strict";
 
@@ -14,4 +13,34 @@ $(function() {
     $(window).on('scroll', function() {
         menuscroll();
     });
-}); 
+});
+
+
+
+$('area[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1');
+                        $target.focus();
+                    }
+                });
+            }
+        }
+    });
